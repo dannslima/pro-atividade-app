897c9800f3c4f1877df76397f6597d78b5acc718
Danilo Lima <danilo.lima@csjsistemas.com.br>
2022-10-31 17:23:28 -0300

    d

--- src/App.js
+++ src/App.js
@@ -33,11 +33,27 @@ function App() {
     <>
       <form className="row g-3">
         <div className="col-md-6">
-          <label for='inputEmail4' className="form-label">Id</label>
+          <label className="form-label">
+            Id
+          </label>
+
           <input id='id' type="text" className="form-control" />
         </div>
+        <div class="col-md-6">
+          <label className="form-label">Prioridade</label>
+          <select id="inputState" className="form-select">
+            <option defaultValue='0'>Selecionar...</option>
+            <option value='1'>Baixa</option>
+            <option value='2'>Normal</option>
+            <option value='3'>Alta</option>
+          </select>
+        </div>
         <div className="col-md-6">
-          <label for='inputEmail4' className="form-label">Descricao</label>
+          <label className="form-label">Título</label>
+          <input id='título' type="text" className="form-control" />
+        </div>
+        <div className="col-md-6">
+          <label className="form-label">Descricao</label>
           <input id='descricao' type="text" className="form-control" />
         </div>
         <hr />
@@ -49,35 +65,37 @@ function App() {
         <b />
       </form>
       <div className="mt-3">
-          {atividades.map(ativ => (
-            <div key={ativ.id} className="card mb-2 shadow-sm" style={{ idth: '18rem' }}>
-              <div class="card-body">
-                <div className='d-flex justify-content-between'>
-                  <h5 className='card-title'>
-                    <span className='badge bg-secondary me-1'>
-                      {ativ.id}
-                    </span>
-                    - título
-                  </h5>
-                  <h6> Prioridade:
-                    <span className='ms-1 text-black'>
-                       <i className="me-1 far fa-meh"  ></i>
-                       Normal
-                    </span>
-                  </h6>
-                </div>
-                <p class="card-text">  {ativ.id} - {ativ.descricao} </p>
-                <div className='d-flex justify-content-end pt-2 m-0 border-top'>
-                  <button className='btn btn-sm btn-outline-primary me-2'>
-                      editar
-                  </button>
-                  <button className='btn btn-sm btn-outline-danger'>
-                      Deletar
-                  </button>
-                </div> 
+        {atividades.map(ativ => (
+          <div key={ativ.id} className="card mb-2 shadow-sm" style={{ idth: '18rem' }}>
+            <div class="card-body">
+              <div className='d-flex justify-content-between'>
+                <h5 className='card-title'>
+                  <span className='badge bg-secondary me-1'>
+                    {ativ.id}
+                  </span>
+                  - título
+                </h5>
+                <h6> Prioridade:
+                  <span className='ms-1 text-black'>
+                    <i className="me-1 far fa-meh"  ></i>
+                    Normal
+                  </span>
+                </h6>
+              </div>
+              <p class="card-text">  {ativ.id} - {ativ.descricao} </p>
+              <div className='d-flex justify-content-end pt-2 m-0 border-top'>
+                <button className='btn btn-sm btn-outline-primary me-2'>
+                  <i className='fas fa-pen me-2'></i>
+                  editar
+                </button>
+                <button className='btn btn-sm btn-outline-danger'>
+                  <i className='fas fa-trash me-2'></i>
+                  Deletar
+                </button>
               </div>
             </div>
-          ))}
+          </div>
+        ))}
       </div>
     </>
   );
