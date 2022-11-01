1f7c8affd70a5050a5f4a4407043b82401b641e1
Danilo Lima <danilo.lima@csjsistemas.com.br>
2022-11-01 17:49:09 -0300

    começando componentes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { useState } from 'react';
 import './App.css';
+import AtividadeForm from './components/AtividadeForm';
+import Atividade from './components/Atividade';
 
 let initialState = [
 
   {
     id: 1,
+    prioridade:'3',
+    titulo: 'titulo',
     descricao: 'Primeira Atividade',
-  },
-  {
-    id: 2,
-    descricao: 'Segunda Atividade',
   },
 
 ];
@@ -23,78 +23,32 @@ function App() {
 
     const atividade = {
       id: document.getElementById('id').value,
+      prioridade: document.getElementById('prioridade').value,
+      titulo: document.getElementById('titulo').value,
       descricao: document.getElementById('descricao').value,
     };
     atividades.push(atividade);
     setAtividades([...atividades, { ...atividade }])
   }
 
+  function deletarAtividade(id){
+    const atividadesFiltradas =  atividades.filter(atividade => atividade.id !== id)
+    setAtividades([...atividadesFiltradas])
+  }
+
   return (
     <>
-      <form className="row g-3">
-        <div className="col-md-6">
-          <label className="form-label">
-            Id
-          </label>
-
-          <input id='id' type="text" className="form-control" />
-        </div>
-        <div class="col-md-6">
-          <label className="form-label">Prioridade</label>
-          <select id="inputState" className="form-select">
-            <option defaultValue='0'>Selecionar...</option>
-            <option value='1'>Baixa</option>
-            <option value='2'>Normal</option>
-            <option value='3'>Alta</option>
-          </select>
-        </div>
-        <div className="col-md-6">
-          <label className="form-label">Título</label>
-          <input id='título' type="text" className="form-control" />
-        </div>
-        <div className="col-md-6">
-          <label className="form-label">Descricao</label>
-          <input id='descricao' type="text" className="form-control" />
-        </div>
-        <hr />
-        <div class="col-12">
-          <button className='btn btn-outline-secondary' onClick={addAtividade}>
-            + Atividade
-          </button>
-        </div>
-        <b />
-      </form>
+      <AtividadeForm 
+        addAtividade={addAtividade}
+        atividades={atividades}
+      />
       <div className="mt-3">
         {atividades.map(ativ => (
-          <div key={ativ.id} className="card mb-2 shadow-sm" style={{ idth: '18rem' }}>
-            <div class="card-body">
-              <div className='d-flex justify-content-between'>
-                <h5 className='card-title'>
-                  <span className='badge bg-secondary me-1'>
-                    {ativ.id}
-                  </span>
-                  - título
-                </h5>
-                <h6> Prioridade:
-                  <span className='ms-1 text-black'>
-                    <i className="me-1 far fa-meh"  ></i>
-                    Normal
-                  </span>
-                </h6>
-              </div>
-              <p class="card-text">  {ativ.id} - {ativ.descricao} </p>
-              <div className='d-flex justify-content-end pt-2 m-0 border-top'>
-                <button className='btn btn-sm btn-outline-primary me-2'>
-                  <i className='fas fa-pen me-2'></i>
-                  editar
-                </button>
-                <button className='btn btn-sm btn-outline-danger'>
-                  <i className='fas fa-trash me-2'></i>
-                  Deletar
-                </button>
-              </div>
-            </div>
-          </div>
+            <Atividade 
+              key={ativ.id}
+              ativ={ativ}
+              deletarAtividade={deletarAtividade}
+            />
         ))}
       </div>
     </>
---
+++ src/components/AtividadeLista.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class AtividadeLista extends Component {
+  render() {
+    return (
+      <div>AtividadeLista</div>
+    )
+  }
+}
